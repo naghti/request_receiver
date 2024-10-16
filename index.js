6d480128176def3140201cb41881f78721cfce90
o <3vefaaaa@mail.ru>
2024-10-16 19:08:40 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const cors = require('cors')
+const app = express()
+const PORT = 5000
+
+app.use(cors());
+app.use(express.json())
+app.set('trust proxy', true)
+app.use('/api', f)
+
+function f (req, res) {
+  console.log(req.query)
+
+  return res.json(true)
+}
+
+const start = async () => {
+  try {
+    app.listen(PORT, () => console.log('SERVER STARTED ON PORT ' + PORT))
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+start()
