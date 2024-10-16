7f5fc3b00aceea5cb186711a645369a97c647805
o <3vefaaaa@mail.ru>
2024-10-16 21:02:39 +0700

    add ssl

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 const express = require("express");
 const cors = require('cors')
+const https = require("https")
 const app = express()
+const fs = require('fs')
+const path = require('path')
 const PORT = process.env.PORT || 5000
 
 app.use(cors());
 app.use(express.json())
 app.set('trust proxy', true)
-app.use('/api', f)
+app.use('/', f)
 
 function f (req, res) {
-  console.log(req.query)
+  console.log(req)
 
-  return res.json(true)
+  res.send("hello")
 }
 
-const start = async () => {
-  try {
-    app.listen(PORT, () => console.log('SERVER STARTED ON PORT ' + PORT))
-  } catch (e) {
-    console.log(e)
-  }
-}
+const sslServer = https.createServer({
+  key: fs.readFileSync(path.join(__dirname, 'cert', 'key.pem')),
+  cert: fs.readFileSync(path.join(__dirname, 'cert', 'cert.pem'))
+}, app)
 
-start()
+sslServer.listen(PORT, () => console.log("server listen on " + PORT))
